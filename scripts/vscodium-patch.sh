868b59961a0e1201663703e73066a99e6eb07858
Darrell Kundel <darrell@demiflat.com>
2023-06-28 15:11:40 -0700

    updates

--- scripts/vscodium-patch.sh
+++ scripts/vscodium-patch.sh
@@ -4,3 +4,4 @@ cp /usr/share/codium/resources/app/product.json product.json.orig
 cp /usr/share/codium/resources/app/product.json product.json.new
 patch -b -u product.json.new -i product.json.patch
 sudo cp product.json.new /usr/share/codium/resources/app/product.json
+sudo cp product.json.new /usr/share/codium/resources/app/product.json.new
